a50bacc70e74aac1f5a29e67d5907a7a56837d3b
yu-nykanorova <juliyasos@ukr.net>
2026-01-29 13:09:29 +0200

    task 2 - added main styles and favicon; task 1 - added favicon

--- task_2/js/index.js
+++ task_2/js/index.js
@@ -11,12 +11,16 @@ function showUsers(users) {
         const userCard = document.createElement("div");
         userCard.classList.add("user-card");
         const id = document.createElement("p");
-        const name = document.createElement("h2");
-        const link = document.createElement("a");
         id.textContent = `User # ${user.id}`;
+        const name = document.createElement("h2");
         name.textContent = `${user.name}`;
+        const link = document.createElement("a");
         link.href = `user-details.html?user=${user.id}`;
         link.textContent = `More details`;
+        const arrow = document.createElement("span");
+        arrow.classList.add("arrow");
+        arrow.textContent = ` >>>`;
+        link.append(arrow);
         userCard.append(id, name, link);
         usersList.append(userCard);
     }
